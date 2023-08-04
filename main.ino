4c856cd60ffb5caaaf54210afaa183e3603ea627
Oskar Wong <atorpos@mac.com>
2023-08-04 19:47:58 -0400

    the project

--- main.ino
+++ main.ino
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <LCDKeypad.h>
 #include <dht.h>
@@ -9,9 +8,10 @@ char words[][10] = { "I", "go", "to", "school", "by", "bus" };
 String display_words = "Waiting the Signal";
 LCDKeypad lcd;
 int numStrings;
+double temp_value = 0;
+double humid_value = 0;
+char buffer[20];
 #define DHT11_PIN 3
-#define DHT21_PIN 5
-#define DHT22_PIN 6
 
 void setup() {
     Serial.begin(9600);
@@ -40,23 +40,42 @@ void loop() {
             Serial.print("Unknown error,\t");
             break;
     }
-    Serial.print(DHT.humidity, 1);
-    Serial.print(",\t");
-    Serial.print(DHT.temperature, 1);
-    digitalWrite(13, HIGH);
-    lcd.setCursor(0, 1);
-    lcd.print("Time:");
-    for (int i = 0; i < 6; i++) {
-        lcd.setCursor(0, 0);
-        lcd.print(words[i]);
-        delay(1000);
-        lcd.setCursor(0, 0);
-        lcd.print("                ");
+    if(chk == DHTLIB_OK) {
+        humid_value = DHT.humidity;
+        temp_value = DHT.temperature;
+        loopingrun(0, 6, dtostrf(temp_value,6,2,buffer));
+        loopingrun(1, 6, dtostrf(humid_value,6,2,buffer));
+        Serial.print(DHT.humidity, 1);
+        Serial.print(",\t");
+        Serial.print(DHT.temperature, 1);
+
     }
+
+    //digitalWrite(13, HIGH);
+    loopingrun(0,0, "Temp: ");
+    loopingrun(1,0, "Humi: ");
+    // for (int i = 0; i < 6; i++) {
+    //   lcd.setCursor(0, 0);
+    //   lcd.print(words[i]);
+    //   delay(1000);
+    //   lcd.setCursor(0, 0);
+    //   lcd.print("                ");
+    // }
     unsigned long currentTimestamp = millis();
 
-    lcd.setCursor(6, 1);
-    lcd.print(currentTimestamp);
-    digitalWrite(13, LOW);
+    // lcd.setCursor(6, 1);
+    // lcd.print(currentTimestamp);
+    // digitalWrite(13, LOW);
+
     delay(1000);
+    // lcd.clear();
+}
+void loopingrun(int x, int y, String displayname) {
+
+    lcd.setCursor(y,x);
+    lcd.print(displayname);
+    //lcd.setCursor(y,1);
+    //lcd.print("Marker");
+    delay(1000);
+
 }
